347a3afb6a710379f8023ae49dfe1029ca9ce9c7
usr <usr@USR-PC>
2016-04-23 18:33:26 +0200

    Working reading from serial and timers

--- ArduinoControl/MainWindow.xaml.cs
+++ ArduinoControl/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO.Ports;
+using System.Timers;
 
 namespace ArduinoControl
 {
@@ -20,9 +22,58 @@ namespace ArduinoControl
     /// </summary>
     public partial class MainWindow : Window
     {
+        SerialPort p1;
+        Timer t1;
+        Timer t2;
+        List<string> a;
         public MainWindow()
         {
             InitializeComponent();
+            p1 = new SerialPort("COM3");
+            p1.Open();
+
+            a = new List<string>();
+
+            t1 = new Timer();
+            t1.Interval = 1000;
+            t1.Elapsed += new ElapsedEventHandler(this.timerX);
+            t1.Start();
+            t2 = new Timer();
+            t2.Interval = 5000;
+            t2.Elapsed += new ElapsedEventHandler(this.timerX2);
+            t2.Start();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void timerX(Object stateInfo, EventArgs e)
+        {
+            string read = p1.ReadExisting();
+            if (read != " "&&read!="")
+            {
+                //textBox.Text = read;
+                //listBox.Items.Add(read);
+                a.Add(read);
+                listBox.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal, new Action(delegate () { listBox.Items.Add(read); }));
+            }
+        }
+        private void timerX2(Object stateInfo, EventArgs e)
+        {
+            string obj = a[0];
+            a.RemoveAt(0);
+
+            if (obj != null && obj != "")
+            {
+
+            }
+        }
+        
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            
+          
         }
     }
 }
